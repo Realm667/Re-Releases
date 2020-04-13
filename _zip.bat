2f34633dee9f6cc74ea55e08e9b1374a1d5a3694
Daniel Gimmer <daniel@dg-media.com>
2020-04-13 17:40:02 +0200

    Uploaded all the other Realm667 exclusive releases to prepare the rejuvination for them as well

--- _zip.bat
+++ _zip.bat
@@ -1,4 +1,15 @@
 @echo off
+7za a -tzip -x!".git" austerity.pk3 .\austerity\*
+7za a -tzip -x!".git" kdizd.pk3 .\kdizd\*
+7za a -tzip -x!".git" sapphire.pk3 .\sapphire\*
 7za a -tzip -x!".git" strnghld.pk3 .\stronghold\*
+7za a -tzip -x!".git" tcotd1.pk3 .\tcotd1\*
 7za a -tzip -x!".git" tcotd2.pk3 .\tcotd2\*
+7za a -tzip -x!".git" therefinery.pk3 .\therefinery\*
+7za a -tzip -x!".git" tnte1.pk3 .\tnte1\*
+7za a -tzip -x!".git" tnte2.pk3 .\tnte2\*
+7za a -tzip -x!".git" tnte3.pk3 .\tnte3\*
+7za a -tzip -x!".git" tntle.pk3 .\tntle\*
 7za a -tzip -x!".git" tutnt.pk3 .\tutnt\*
+7za a -tzip -x!".git" zdcmp1.pk3 .\zdcmp1\*
+7za a -tzip -x!".git" zdcmp2.pk3 .\zdcmp2\*
