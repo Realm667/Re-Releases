b9b99d62d2083aea805584f03b2c7dcd4c2918e1
Daniel Gimmer <daniel@dg-media.com>
2020-05-24 20:24:14 +0200

    "Stronghold": Fixed the batch file generator

--- stronghold_build.bat
+++ stronghold_build.bat
@@ -1,5 +1,5 @@
 @echo off
-7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" strnghld.pk3 .\stronghold\*
+7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" stronghld.pk3 .\stronghold\*
 
 
 
