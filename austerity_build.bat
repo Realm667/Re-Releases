ec61873efa0702196dcdcbf3f89702ea7b876686
Ozymandias81 <Ozymandias81@gmx.com>
2023-10-30 17:06:46 +0100

    Fix all builders and PSD folders
    :new: As the title says, now all re-releases have a #PSD folder, where I have put the # symbol so it gets shown on top of your folder structure (if with Name order usually)
    :new: All builders now share the same features (no compression, exclude #PSD, exclude fonts)

--- austerity_build.bat
+++ austerity_build.bat
@@ -1,3 +1,3 @@
 @echo off
-7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" austerity.pk3 .\austerity.released-v31-20220907\*
+7za a -tzip -mx0 -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" austerity.pk3 .\austerity.released-v31-20220907\*
 
--- tnte1_build.bat
+++ tnte1_build.bat
@@ -1,7 +1,5 @@
 @echo off
-7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tnte1.pk3 .\tnte1\*
-
-
+7za a -tzip -mx0 -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tnte1.pk3 .\tnte1\*
 
 
 
