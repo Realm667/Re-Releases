c686f386b652821921766bea7e9aff53b4b30b2e
Ozymandias81 <Ozymandias81@gmx.com>
2023-10-31 13:05:00 +0100

    Minor changes
    BUILDERS & ZPACK
    
    :new: Updated TCOTD2 builder with new folder
    :new: Removed backend override for ZPack, this way we also support ZGles

--- tcotd2_build.bat
+++ tcotd2_build.bat
@@ -1,5 +1,5 @@
 @echo off
-7za a -tzip -mx0 -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tcotd2.pk3 .\tcotd2\*
+7za a -tzip -mx0 -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!tools -xr!#PSD -x!.vscode\ -xr!"*.bat" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tcotd2.pk3 .\tcotd2.released-v20-20231031\*
 
 
 
