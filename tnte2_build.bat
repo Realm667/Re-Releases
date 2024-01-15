8922d72fc223d38513bc6de205dd66c137939004
Daniel Gimmer <daniel@dg-media.com>
2024-01-15 17:16:36 +0100

    [TNT2] Finalizing, onto TNT3 now

--- tnte2_build.bat
+++ tnte2_build.bat
@@ -1,5 +1,5 @@
 @echo off
-7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tnte2.pk3 .\tnte2\*
+7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tnte2.pk3 .\tnte2.released_v20-20240115\*
 
 
 
