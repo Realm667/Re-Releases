aa3738db3d61b4600038bb00644dcfde4e0bd455
Ozymandias81 <Ozymandias81@gmx.com>
2024-03-07 18:40:10 +0100

    Wrong batch file changed for TNTLE release
    BUILDERS
    
    :new: Now it is set properly for both TNTE3 and TNTLE

--- tnte3_build.bat
+++ tnte3_build.bat
@@ -1,5 +1,5 @@
 @echo off
-7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tnte3.pk3 .\tntle.released_v20-20240306\*
+7za a -tzip -x!".git" -xr!"*.dbs" -xr!"*.backup1" -xr!"*.backup2" -xr!"*.backup3" -xr!"*.psd" -xr!"*.otf" -xr!"*.ttf" tnte3.pk3 .\tnte3.released_v20-20240129\*
 
 
 
